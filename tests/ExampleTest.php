46b3bf4d8ae5eacf0c5c273c9c5cb3d5f11753de
novalramdhani <novalfitrahramdhani@gmail.com>
2022-08-24 01:01:07 +0700

    add test example

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase 
+{
+    public function testIfTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
